2f95e6cd49eb6b2e98773120bc033eb36929f159
EUROSAI <eurosai.eu@gmail.com>
2025-07-03 15:20:33 +0000

    Браузеры пока не распознают, что можно это установить как приложение.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { I18nProvider } from '@/hooks/use-i18n';
+import { ServiceWorkerRegistrar } from '@/components/service-worker-registrar';
 
 export const metadata: Metadata = {
   title: 'Facebook AI',
@@ -30,6 +31,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <I18nProvider>
+          <ServiceWorkerRegistrar />
           {children}
           <Toaster />
         </I18nProvider>
